993549be39d040c8d46256eb74c949b206078c18
Sam Holiday <142548008+holidaysam@users.noreply.github.com>
2024-02-27 22:43:48 +0000

    Tue, Feb 27, 2024, 2:43 PM -08:00

--- js/loading.js
+++ js/loading.js
@@ -1,6 +1,14 @@
 loadText = document.querySelector('.loading-text')
+bg = document.querySelector('.bg')
 
 load = 0
 
-loadText.innerText = `${load}%`
-loadText.style.opacity = 0.5
+blurring()
+
+
+function blurring() {
+    load++
+    loadText.innerText = `${load}%`
+    loadText.style.opacity = 1
+    bg.style.filter = "blur(10px)"
+}
