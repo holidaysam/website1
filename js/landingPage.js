a5954155ba2b2e94ee510eae5902fcfbaf84a300
Sam Holiday <142548008+holidaysam@users.noreply.github.com>
2024-03-10 06:29:34 +0000

    Sat, Mar 9, 2024, 10:29 PM -08:00

--- js/landingPage.js
+++ js/landingPage.js
@@ -3,14 +3,24 @@ open = document.getElementById('open')
 modal = document.getElementById('modal')
 close = document.getElementById('close')
 
+// toggle nav bar
 toggle.addEventListener('click', () => {
     document.body.classList.toggle('show-nav')
 })
 
+// show the modal
 open.addEventListener('click', () => {
     modal.classList.add('show-modal')
 })
 
+// hide the modal
 close.addEventListener('click', () => {
     modal.classList.remove('show-modal')
+})
+
+//hide the modal on outside click
+window.addEventListener('click', (e) => {
+    if (e.target == modal) {
+        modal.classList.remove('show-modal')
+    }
 })
