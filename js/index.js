8af389cd29398340c90003f0725a6e083d8a7140
Sam Holiday <142548008+holidaysam@users.noreply.github.com>
2024-03-12 21:24:40 +0000

    Tue, Mar 12, 2024, 2:24 PM -07:00

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@ nav = document.querySelector('.nav')
 window.addEventListener('scroll', fixNav)
 
 function fixNav() {
-    if (window.scrollY > 50) {
+    if (window.scrollY > 100) {
         nav.classList.add('active')
     } else {
         nav.classList.remove('active')
